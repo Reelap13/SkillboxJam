ec0491bbf5edb45699467fc8bc2867987f7d9e5d
unknown <probka2992@gmail.com>
2023-05-06 20:07:16 +0300

    score

--- Circle RL/Assets/Object/GameBoard/GameConditionController/Cards/Card.cs
+++ Circle RL/Assets/Object/GameBoard/GameConditionController/Cards/Card.cs
@@ -6,8 +6,11 @@ public class Card : MonoBehaviour
 {
     [SerializeField] public string description;
     [SerializeField] public GameObject objectToSpawn;
+    [SerializeField] int score;
+    static public int globaleScore = 0;
     public void choose()
     {
+        globaleScore += score;
         Debug.Log(description);
         ObjectPlacer.Instance.AddObject(objectToSpawn);
     }
--- Circle RL/Assets/Object/GameBoard/GameConditionController/RestartButton.cs
+++ Circle RL/Assets/Object/GameBoard/GameConditionController/RestartButton.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartButton : MonoBehaviour
 {
@@ -10,7 +12,11 @@ public class RestartButton : MonoBehaviour
     public void showOn()
     {
         if (_ph.HitPoint <= 0)
+        {
+            PlayerPrefs.SetInt("Score",max(PlayerPrefs.GetInt("Score", 0)), Card.globaleScore);
+            GetComponentInChildren<TextMeshProUGUI>().text = "Your score:" + Card.globaleScore + "\nRestart?";
             gameObject.SetActive(true);
+        }
     }
     public void resetLevel()
     {
