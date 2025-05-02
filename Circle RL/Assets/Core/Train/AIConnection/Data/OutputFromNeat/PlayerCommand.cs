3d4b59eeafc55de6cc96c973eab1cccf2207951b
Reelap13 <116920397+Reelap13@users.noreply.github.com>
2025-05-02 14:19:51 +0300

    feature: fast fixes

--- Circle RL/Assets/Core/Train/AIConnection/Data/OutputFromNeat/PlayerCommand.cs
+++ Circle RL/Assets/Core/Train/AIConnection/Data/OutputFromNeat/PlayerCommand.cs
@@ -4,10 +4,7 @@ namespace Train.AIConnection.Data
 {
     public class PlayerCommand
     {
-        public bool W;
-        public bool A;
-        public bool S;
-        public bool D;
+        public Coordinates Direction;
         public bool W1;
         public bool W2;
         public bool W3;
--- Circle RL/Assets/Core/Train/Player/PlayerAIBehaviour.cs
+++ Circle RL/Assets/Core/Train/Player/PlayerAIBehaviour.cs
@@ -11,18 +11,7 @@ namespace Train.Player
 
         public void ProcessCommand(PlayerCommand command)
         {
-            Player.Movement.SetDirection(ParseDirection(command));
-        }
-
-        private Vector2 ParseDirection(PlayerCommand command)
-        {
-            Vector2 direction = Vector2.zero;
-            if (command.W) direction += Vector2.up;
-            if (command.A) direction += Vector2.left;
-            if (command.S) direction += Vector2.down;
-            if (command.D) direction += Vector2.right;
-
-            return direction.normalized;
+            Player.Movement.SetDirection(command.Direction);
         }
     }
 }
